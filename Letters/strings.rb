5fdd4c11e8d118271eff32a11e9dcad5ef155ee0
claudiug <claudiu.garba@gmail.com>
2014-03-20 21:38:24 +0100

    Initial commit

---
+++ Letters/strings.rb
@@ -0,0 +1,37 @@
+
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye!'
+
+#Adding strings with no space:
+puts 'I like' + 'apple pie.'
+
+#Adding strings with spaces:
+puts 'I like ' + 'apple pie.'
+puts 'I like' + ' apple pie.'
+
+# Multiplying strings:
+puts 'blink ' * 4
+
+# Difference between numbers and digits
+
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+# Problems = things that didn't work
+
+#1. Add a number to a string puts '12' + 12
+#2. Multiply strings puts '2' * '5'
+#3. Order of multiplying matters puts 5*'pig'
+#4. Escape the apostrophe. This don't work: puts 'you're swell!'. But this does:
+    puts 'You\'re swell!'
+#5. Examples of escaping in strings:
+    puts 'You\'re swell!'
+    puts 'backslash at the end of a string: \\'
+    puts 'up\\down'
+    puts 'up\down'
---
+++ Numbers/exercices02.rb
@@ -0,0 +1,16 @@
+
+# Hours in a year. How many hours are in a year?
+
+puts "A year has #{365*24} hours, or #{366*24} hours if it's a leap year."
+
+# Minutes in a decade. How many minutes are in a decade?
+
+puts "A decade has #{60*24*(365*10+2)} or #{60*24*(365*10+3)} minutes."
+
+# Your age in seconds. How many seconds old are you?
+
+puts "I am #{60*60*24*(365*34 + (34/4))} seconds old."
+
+# If I'm 1.160 million seconds old, how old am I?
+
+puts  "You have #{1160000000/(60*60*24*365)} years."
