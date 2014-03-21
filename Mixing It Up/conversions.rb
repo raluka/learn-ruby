c044d8f50183c7f2a922eea28e9ab8bcb38595d0
raluka <badoi.raluca@gmail.com>
2014-03-21 17:37:49 +0100

    talking with my computer

---
+++ Mixing It Up/conversions.rb
@@ -0,0 +1,41 @@
+# String version of an object .to_s
+
+var1 = 2
+var2 = '5'
+puts var1.to_s + var2
+
+# Object to integer .to_i
+
+var1 = 2
+var2 = '5'
+puts var1.to_s + var2
+puts var1 + var2.to_i
+puts ''
+
+# More conversions
+
+puts '15'.to_f
+puts '99.999'.to_f
+puts '99.999'.to_i
+puts ''
+puts '5 is my favorite number!'.to_i
+puts 'Who asked about 5 or whatever?'.to_i
+puts 'Your momma did.'.to_f
+puts ''
+puts 'stringy'.to_s
+puts 3.to_i
+
+# gets
+
+puts 'Hello there, and what\'s your name?'
+name = gets
+puts 'Your name is ' + name + '? What a lovely name!'
+puts 'Pleased to meet you, ' + name + '. :)'
+
+# and chomp
+
+puts 'Hello there, and what\'s your name?'
+name = gets.chomp
+puts 'Your name is ' + name + '? What a lovely name!'
+puts 'Pleased to meet you, ' + name + '. :)'
+
---
+++ Mixing It Up/name.rb
@@ -0,0 +1,11 @@
+
+# Full name greeting. Write a program that asks for a person's first name, then middle, and then last.
+#Finally, it should greet the person using their full name.
+puts 'Hello there, and what\'s your name?'
+first_name = gets.chomp
+puts 'And your middle name? If you don\'t have one, just press Enter.'
+middle_name = gets.chomp
+puts 'And your last name? What\'s you family name?'
+last_name = gets.chomp
+puts "Nice to meet you, #{first_name} #{middle_name} #{last_name}!"
+puts 'You have a lovely name!'
